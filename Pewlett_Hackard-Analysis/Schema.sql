532a624ef99f04cc60599222c2d91c9603f07b6e
unknown <mateoarul@gmail.com>
2021-06-13 23:50:35 -0500

    update schema

--- Pewlett_Hackard-Analysis/Schema.sql
+++ Pewlett_Hackard-Analysis/Schema.sql
@@ -207,4 +207,61 @@ FROM retirement_info as ri
 INNER JOIN dept_employee as de
 ON (ri.emp_no = de.emp_no)
 WHERE de.dept_no = ('d007');
- 
+
+-------------------------DELIVERABLE 1---------------
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT e.emp_no,e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO Dept_Retirees
+FROM employee as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC
+;
+SELECT * FROM Dept_Retirees;
+DROP TABLE Dept_Retirees;
+
+
+----- DISTINCT TILES
+SELECT DISTINCT ON (e.emp_no) e.emp_no,e.first_name,
+e.last_name,
+t.title
+INTO unique_titles
+FROM employee as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC, t.to_date DESC
+;
+DROP TABLE unique_titles;
+
+SELECT COUNT(title), title
+INTO Retitiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER by count(title) DESC;
+
+SELECT * FROM Retitiring_titles ;
+
+
+--------------MENTOR ELIGIBLE
+SELECT DISTINCT ON (e.emp_no) e.emp_no,e.first_name,
+e.last_name, e.birth_date,
+de.from_date, de.to_date, t.title
+INTO mentorship_eligible
+FROM employee as e 
+INNER JOIN dept_employee as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t 
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND  (t.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC, t.title DESC;
+SELECT * FROM mentorship_eligible;
+
+DROP TABLE mentorship_eligible;
+
